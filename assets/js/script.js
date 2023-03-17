07d07f75c98a04de61c3b220327e8a0b9445babd
Riley O'Neil <roneilwork@gmail.com>
2023-03-16 18:43:12 -0700

    added some things

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,5 +31,21 @@ const testFetch = function() {
         .catch(err => console.error(err));
 }
 
+const testStream = function() {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '90bf4e7b22msh25f3182fa016740p1129d4jsn534257354705',
+      'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
+    }
+  };
+  
+  fetch('https://streaming-availability.p.rapidapi.com/v2/search/title?title=batman&country=us&type=movie&output_language=en', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+}
+
 // getFetch();
-testFetch();
+// testFetch();
+testStream();
